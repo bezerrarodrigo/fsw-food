b6291bfba6bac734ff5fbf1d630fa302dd109d2b
Rodrigo Bezerra <3rd.rodrigo.bezerra@spcgrafeno.com.br>
2026-02-27 15:15:11 -0300

    feat: implement Header component and update Home page layout

---
+++ app/components/header.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button";
+import { MenuIcon } from "lucide-react";
+import Image from "next/image";
+
+const Header = () => {
+  return (
+    <div className="flex justify-between items-center p-5">
+      <Image src="/logo.png" alt="Logo" width={100} height={30} />
+      <Button variant="outline" className="border-none bg-transparent">
+        <MenuIcon className="w-6 h-6" />
+      </Button>
+    </div>
+  );
+};
+
+export default Header;
