c1a73bb76b8fd9724d30c98e557e805645d27220
Rodrigo Bezerra <3rd.rodrigo.bezerra@spcgrafeno.com.br>
2026-03-03 10:55:38 -0300

    feat: update Header and Home components for improved styling; add promotional banner image

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import CategoryList from "./components/category-list";
 import Header from "./components/header";
 import Search from "./components/search";
@@ -9,8 +10,19 @@ export default function Home() {
       <div className="px-5">
         <Search />
       </div>
-      <div className="px-5 pt-4">
+      <div className="px-5 pt-6">
         <CategoryList />
+      </div>
+      <div className="px-5 pt-6">
+        <Image
+          className="w-full h-auto object-contain"
+          src="/banner-promo01.png"
+          alt="Até 30% de desconto em pizzas"
+          width={0}
+          height={0}
+          sizes="100vw"
+          quality={100}
+        />
       </div>
     </>
   );
