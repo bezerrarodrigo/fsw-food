6a3b8ce7b390bd9af78513b419a89bff4b61f180
Rodrigo Bezerra <bezerra.rodrigo@gmail.com>
2026-05-11 17:58:26 -0300

    feat: implement AlertDialog component and integrate into Cart for order confirmation

--- app/components/cart.tsx
+++ app/components/cart.tsx
@@ -1,15 +1,37 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { CartContext } from "../contexts/cart";
 import CartItem from "./cart-item";
 import { createOrder } from "../actions/order";
 import { useSession } from "next-auth/react";
+import { Loader2 } from "lucide-react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 const Cart = () => {
+  //state
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [showDialog, setShowDialog] = useState(false);
+
   //contexts
-  const { products, subtotalPrice, totalPrice, totalDiscount, deliveryFee } =
-    useContext(CartContext);
+  const {
+    products,
+    subtotalPrice,
+    totalPrice,
+    totalDiscount,
+    deliveryFee,
+    clearCart,
+  } = useContext(CartContext);
 
   const { data } = useSession();
 
@@ -19,84 +41,118 @@ const Cart = () => {
 
     const restaurant = products[0]?.restaurant;
 
-    await createOrder({
-      subtotalPrice,
-      totalPrice,
-      totalDiscounts: totalDiscount,
-      deliveryFee: restaurant.deliveryFee,
-      deliveryTime: restaurant.deliveryTime,
-      restaurant: {
-        connect: { id: restaurant.id },
-      },
-      status: "CONFIRMED",
-      user: {
-        connect: { id: data.user.id },
-      },
-    });
+    try {
+      setIsSubmitting(true);
+      await createOrder({
+        subtotalPrice,
+        totalPrice,
+        totalDiscounts: totalDiscount,
+        deliveryFee: restaurant.deliveryFee,
+        deliveryTime: restaurant.deliveryTime,
+        restaurant: {
+          connect: { id: restaurant.id },
+        },
+        status: "CONFIRMED",
+        user: {
+          connect: { id: data.user.id },
+        },
+      });
+      clearCart();
+    } catch (error) {
+      console.error("Error creating order:", error);
+    } finally {
+      setIsSubmitting(false);
+    }
   }
 
   return (
-    <div className="flex flex-col justify-between">
-      <div>
-        {products.map((product) => (
-          <CartItem key={product.id} cartProduct={product} />
-        ))}
-      </div>
-      <div className="p-5 mt-auto">
-        <Card>
-          <CardContent>
-            <div className="flex justify-between items-center border-b pb-2">
-              <span className="text-xs text-muted-foreground">Subtotal:</span>
-              <span className="text-xs">
-                {Intl.NumberFormat("pt-BR", {
-                  style: "currency",
-                  currency: "BRL",
-                }).format(subtotalPrice)}
-              </span>
-            </div>
+    <>
+      <div className="flex flex-col justify-between">
+        <div>
+          {products.map((product) => (
+            <CartItem key={product.id} cartProduct={product} />
+          ))}
+        </div>
+        <div className="p-5 mt-auto">
+          <Card>
+            <CardContent>
+              <div className="flex justify-between items-center border-b pb-2">
+                <span className="text-xs text-muted-foreground">Subtotal:</span>
+                <span className="text-xs">
+                  {Intl.NumberFormat("pt-BR", {
+                    style: "currency",
+                    currency: "BRL",
+                  }).format(subtotalPrice)}
+                </span>
+              </div>
 
-            <div className="flex justify-between items-center border-b pb-2 mt-2">
-              <span className="text-xs text-muted-foreground">Entrega:</span>
-              <span
-                className={`text-xs ${deliveryFee === 0 ? "text-red-500" : ""}`}
+              <div className="flex justify-between items-center border-b pb-2 mt-2">
+                <span className="text-xs text-muted-foreground">Entrega:</span>
+                <span
+                  className={`text-xs ${deliveryFee === 0 ? "text-red-500" : ""}`}
+                >
+                  {deliveryFee === 0
+                    ? "Grátis"
+                    : Intl.NumberFormat("pt-BR", {
+                        style: "currency",
+                        currency: "BRL",
+                      }).format(deliveryFee)}
+                </span>
+              </div>
+
+              <div className="flex justify-between items-center mt-2 border-b pb-2">
+                <span className="text-xs text-muted-foreground">Desconto:</span>
+                <span className="text-xs text-red-500">
+                  -
+                  {Intl.NumberFormat("pt-BR", {
+                    style: "currency",
+                    currency: "BRL",
+                  }).format(totalDiscount)}
+                </span>
+              </div>
+              <div className="flex justify-between items-center mt-2 font-bold text-lg">
+                <span className="text-xs text-muted-foreground">Total:</span>
+                <span className="text-xs">
+                  {Intl.NumberFormat("pt-BR", {
+                    style: "currency",
+                    currency: "BRL",
+                  }).format(totalPrice)}
+                </span>
+              </div>
+            </CardContent>
+            <div className="px-4">
+              <Button
+                disabled={isSubmitting}
+                className="w-full"
+                onClick={() => setShowDialog(true)}
               >
-                {deliveryFee === 0
-                  ? "Grátis"
-                  : Intl.NumberFormat("pt-BR", {
-                      style: "currency",
-                      currency: "BRL",
-                    }).format(deliveryFee)}
-              </span>
+                {isSubmitting && <Loader2 className="animate-spin" />} Finalizar
+                pedido
+              </Button>
             </div>
-
-            <div className="flex justify-between items-center mt-2 border-b pb-2">
-              <span className="text-xs text-muted-foreground">Desconto:</span>
-              <span className="text-xs text-red-500">
-                -
-                {Intl.NumberFormat("pt-BR", {
-                  style: "currency",
-                  currency: "BRL",
-                }).format(totalDiscount)}
-              </span>
-            </div>
-            <div className="flex justify-between items-center mt-2 font-bold text-lg">
-              <span className="text-xs text-muted-foreground">Total:</span>
-              <span className="text-xs">
-                {Intl.NumberFormat("pt-BR", {
-                  style: "currency",
-                  currency: "BRL",
-                }).format(totalPrice)}
-              </span>
-            </div>
-          </CardContent>
-          <div className="px-4">
-            <Button className="w-full" onClick={handleFinishOrderClick}>
-              Finalizar pedido
-            </Button>
-          </div>
-        </Card>
+          </Card>
+        </div>
       </div>
-    </div>
+      <AlertDialog open={showDialog} onOpenChange={setShowDialog}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>
+              Deseja realmente finalizar o pedido?
+            </AlertDialogTitle>
+            <AlertDialogDescription>
+              Esta ação não pode ser desfeita. Isso irá finalizar seu pedido
+              permanentemente.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancelar</AlertDialogCancel>
+            <AlertDialogAction onClick={handleFinishOrderClick}>
+              Finalizar
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
   );
 };
 
